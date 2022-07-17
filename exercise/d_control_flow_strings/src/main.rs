7ccee99002ef41ffa82b5e00189865688c15d41f
grahovsky <grahovsky@gmail.com>
2022-07-17 20:33:16 +0300

    exercise d done

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -14,23 +14,36 @@ fn main() {
 
     // This consumes the `args` vector to iterate through each String
     for arg in args {
+
+        println!{"{}", arg}
         // 1a. Your task: handle the command-line arguments!
         //
         // - If arg is "sum", then call the sum() function
         // - If arg is "double", then call the double() function
         // - If arg is anything else, then call the count() function, passing "arg" to it.
 
+        if arg == "sum" {
+            sum();
+        } else if arg == "double" {
+            double();
+        } else {
+            count(arg);
+        } 
 
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
         // after "cargo run".  For example "cargo run sum"
     }
 }
 
-fn sum() {
+pub fn sum() {
     let mut sum = 0;
     // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
+
+    for i in 7..=23 {
+        sum += i
+    }
 
 
     println!("The sum is {}", sum);
@@ -43,6 +56,10 @@ fn double() {
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
 
+    while x < 500 {
+        x = x * 2;
+        count += 1;
+    }
 
     println!("You can double x {} times until x is larger than 500", count);
 }
@@ -53,6 +70,14 @@ fn count(arg: String) {
     //
     // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
 
+    let mut count = 1;
 
-    println!(); // This will output just a newline at the end for cleanliness.
+    'banana: loop {
+
+        println!("{} ", arg); // This will output just a newline at the end for cleanliness.
+        
+        count += 1;
+        if count == 8 { break 'banana }
+
+    }
 }
